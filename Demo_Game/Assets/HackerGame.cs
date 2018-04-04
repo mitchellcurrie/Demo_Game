8032d3952e69a6894798430874e27383d2f0bb12
mitchellcurrie <mitchell.jc@hotmail.com>
2018-04-05 10:36:28 +1200

    Add functions for assigning random strings and displaying them to the screen, add game string struct

--- Demo_Game/Assets/HackerGame.cs
+++ Demo_Game/Assets/HackerGame.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+// Each line of the game is made up of a GameString struct
+struct GameString
+{
+    public string stringText;
+    public bool hasWordPlaced;
+}
+
 public class HackerGame : MonoBehaviour {
 
     public GameManager gameManager;
@@ -12,9 +19,20 @@ public class HackerGame : MonoBehaviour {
     public TextMeshProUGUI playerInputString;
     private int stringCharLimit;
 
+    // Strings
+    public TextMeshProUGUI gameStrings;
+    private GameString[] gameStringArray = new GameString[10];
+    public string Answer;
+    public string[] FakeAnswers;    
+    private string randomChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()-+={}[]|:;<>,.?/";
+    private int gameStringLength;
+
     void Start ()
     {
         stringCharLimit = 10;
+        gameStringLength = 30;
+        SetRandomStrings();
+        DisplayGameStrings();
     }
 	
 	void Update ()
@@ -25,6 +43,7 @@ public class HackerGame : MonoBehaviour {
         }
 	}
 
+    // Check player keyboard input and display to screen by updating on screen text
     void CheckPlayerInputAndDisplay()
     {
         if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
@@ -54,4 +73,37 @@ public class HackerGame : MonoBehaviour {
                 playerInputString.text += Input.inputString;
         }
     }
+
+    void GameSetup()
+    {
+
+    }
+
+    // Display game strings to screen by updating the on screen text
+    void DisplayGameStrings()
+    {
+        gameStrings.text = "";
+
+        foreach (GameString gs in gameStringArray)
+        {
+            gameStrings.text += gs.stringText + "\n";
+        }      
+    }
+
+    // Assign random characters from "randomChars" string to each of the game strings in the array
+    void SetRandomStrings()
+    {
+        for (int i = 0; i < gameStringArray.Length; i++)
+        {
+            gameStringArray[i].stringText = "";
+
+            for (int j = 0; j < gameStringLength; j++)
+            {
+                gameStringArray[i].stringText += randomChars[Random.Range(0, randomChars.Length - 1)];
+            }
+        }
+    }
 }
+
+
+//  TestInt = TestString.IndexOf(Answer);
